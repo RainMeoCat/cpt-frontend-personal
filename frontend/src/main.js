687c7fd86ff8d80384fd3cfb33e2861af5d800ff
RainMeoCat <akimotoming@gmail.com>
2021-07-10 00:03:54 +0800

    完善手機版(Mobile.vue)的按需引入

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { Button, Icon, Row, Col, Menu, MenuItem, Divider, Empty } from 'element-ui'
+import { Button, Icon, Row, Col, Menu, MenuItem, Divider, Empty, Collapse, CollapseItem, Link, Timeline, TimelineItem, Progress, Scrollbar, Drawer } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import animated from 'animate.css'
 
@@ -9,6 +9,13 @@ import animated from 'animate.css'
 
 Vue.use(animated)
 Vue.use(Button)
+Vue.use(Collapse)
+Vue.use(CollapseItem)
+Vue.use(Timeline)
+Vue.use(Link)
+Vue.use(TimelineItem)
+Vue.use(Scrollbar)
+Vue.use(Progress)
 Vue.use(Icon)
 Vue.use(Row)
 Vue.use(Col)
@@ -16,6 +23,7 @@ Vue.use(Menu)
 Vue.use(MenuItem)
 Vue.use(Divider)
 Vue.use(Empty)
+Vue.use(Drawer)
 
 Vue.config.productionTip = false
 
