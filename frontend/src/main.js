ec03aee88fa8712f22211b138e1ddfc3ac6b97c6
RainMeoCat <akimotoming@gmail.com>
2021-07-09 23:32:10 +0800

    babel與chart.js安裝，按需引入設定

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'
+import { Button, Icon, Row, Col, Menu, MenuItem, Divider, Empty } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import animated from 'animate.css'
 
+// 按需引入名單：
+
 Vue.use(animated)
-Vue.use(ElementUI)
+Vue.use(Button)
+Vue.use(Icon)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Divider)
+Vue.use(Empty)
+
 Vue.config.productionTip = false
 
 new Vue({
