e992e5206115010127056460b3bb4284c15c3097
RainMeoCat <akimotoming@gmail.com>
2021-07-11 03:00:04 +0800

    移除animated.css module改以CDN的方式引入，解決build後架站發生的錯誤

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,9 @@ import App from './App.vue'
 import router from './router'
 import { Button, Icon, Row, Col, Menu, MenuItem, Divider, Empty, Collapse, CollapseItem, Link, Timeline, TimelineItem, Progress, Scrollbar, Drawer, Loading } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import animated from 'animate.css'
 
 // 按需引入名單：
 
-Vue.use(animated)
 Vue.use(Button)
 Vue.use(Collapse)
 Vue.use(CollapseItem)
