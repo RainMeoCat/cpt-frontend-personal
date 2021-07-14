1249d6b35835af79f8900e51ad872da5e4c4fb36
tsenghc <danny2953053@gmail.com>
2021-07-14 16:39:18 +0800

    增加簡章全文 附件四及圖表滾動窗格

--- frontend/src/views/Mobile.vue
+++ frontend/src/views/Mobile.vue
@@ -169,6 +169,7 @@
               評審委員在每組中評選16隊晉級決賽(視狀況酌予增減)
             </el-collapse-item>
             <el-collapse-item title="【初賽】評分項目">
+              <el-scrollbar style="height: 60vmin" tag="ul">
               <div style="height: 80vh; overflow-y: scroll; overflow-x: hidden">
                 <div
                   style="text-align: center; font-size: 16px; font-weight: 900"
@@ -183,15 +184,50 @@
                 </div>
                 <Pie-chart :chart-data="dataI" :options="options"></Pie-chart>
               </div>
+              </el-scrollbar>
             </el-collapse-item>
-            <el-collapse-item title="【決賽】交件說明與評分項目">
-              概念組請繳交簡報<el-link
-                type="primary"
-                href="./attach_3.pdf"
-                target="_blank"
-                >(附件三)</el-link
-              ><br />
-              實作組不須繳交資料，若需使用簡報報告大會亦無限定格式，請自行發揮，相關設備也請自行攜帶
+            <el-collapse-item title="【決賽】交件說明"
+              ><div class="sub-item">
+                概念組請繳交簡報<el-link
+                  type="primary"
+                  href="./attach_3.pdf"
+                  target="_blank"
+                  >(附件三)</el-link
+                ><br />
+                實作組不須繳交資料，若需使用簡報報告大會亦無限定格式，請自行發揮，相關設備也請自行攜帶
+              </div></el-collapse-item
+            >
+            <el-collapse-item title="【決賽】比賽規定"
+              ><div class="sub-item">
+                概念組<br />
+                評審針對參賽者簡報內容及表現進行評分。每隊4分鐘，問答時間3分鐘(統問統答)，簡報統一由大會電腦投影。
+                <el-divider></el-divider>
+                實作組<br />
+                各隊展示、解說作品(展場規定請參照<el-link
+                  type="primary"
+                  href="./attach_4.pdf"
+                  target="_blank"
+                  >(附件四)</el-link
+                >)，由評審針對作品及報告進行評分，每隊10分鐘，問答時間5分鐘(統問統答)。
+              </div></el-collapse-item
+            >
+            <el-collapse-item title="【決賽】評分項目">
+              <el-scrollbar style="height: 60vmin" tag="ul">
+              <div>
+                <div
+                  style="text-align: center; font-size: 16px; font-weight: 900"
+                >
+                  概念組
+                </div>
+                <Pie-chart :chart-data="dataC2" :options="options"></Pie-chart>
+                <div
+                  style="text-align: center; font-size: 16px; font-weight: 900"
+                >
+                  實作組
+                </div>
+                <Pie-chart :chart-data="dataI2" :options="options"></Pie-chart>
+              </div>
+              </el-scrollbar>
             </el-collapse-item>
             <el-collapse-item>
               <template slot="title"
@@ -276,6 +312,18 @@
                   >開發團隊/網頁維護：ITALAB</span
                 >
               </div>
+            </el-collapse-item>
+            <el-collapse-item>
+              <template slot="title"
+                ><i class="header-icon el-icon-info"></i>
+                簡章全文
+              </template>
+              <el-link
+                  type="primary"
+                  href="./cpt_guide.pdf"
+                  target="_blank"
+                  >點擊下載簡章全文</el-link
+                ><br />
             </el-collapse-item>
           </el-collapse>
         </div>
@@ -454,31 +502,74 @@ export default {
           timestamp: '2021-10-24'
         }
       ],
-      award_info: [ // 競賽辦法.獎勵辦法
-        {
-          data: '獎品項目暫訂，主辦單位得依實際狀況更改之。'
-        },
-        {
-          data: '競賽得獎者，於推薦甄試高雄科技大學商業智慧學院各系，可列為多元表現加分項目。'
-        },
-        {
-          data: '所有參賽者，皆頒發參賽證明，可上傳作為推薦甄試能力佐證資料。'
-        }
-      ],
-      idea_award: [ // 競賽辦法.獎勵辦法
-        {
-          data: '特優一名，團體獎金10,000元、每人獎狀一只'
-        },
-        {
-          data: '優等一名，團體獎金5,000元、每人獎狀一只'
-        },
-        {
-          data: '佳作五名，團體獎金2,000元、每人獎狀一只'
-        },
-        {
-          data: '特別獎數名，每人獎狀一只'
-        }
-      ],
+      dataC: { // 競賽辦法.初賽評分項目.概念組
+        labels: ['作品創作理念', '提案創新性', '市場應用可行性', '報告完整度', '預期效益'],
+        datasets: [
+          {
+            data: [10, 30, 30, 10, 20],
+            backgroundColor: [
+              'rgb(66, 129, 164)',
+              'rgb(72, 169, 166)',
+              'rgb(228, 223, 218)',
+              'rgb(212, 180, 131)',
+              'rgb(193, 102, 107)'
+            ],
+            borderColor: '#000',
+            options: this.options
+          }
+        ]
+      },
+      dataI: { // 競賽辦法.初賽評分項目.實作組
+        labels: ['作品創作理念', '作品功能', '市場應用可行性', '成本分析', '實用價值/商業價值'],
+        datasets: [
+          {
+            data: [30, 20, 20, 20, 10],
+            backgroundColor: [
+              'rgb(21, 96, 100)',
+              'rgb(0, 196, 154)',
+              'rgb(248, 225, 108)',
+              'rgb(255, 194, 180)',
+              'rgb(251, 143, 103)'
+            ],
+            borderColor: '#000',
+            options: this.options
+          }
+        ]
+      },
+      dataC2: { // 競賽辦法.決賽評分項目.概念組
+        labels: ['簡報內容', '現場表達能力', '問題回覆', '簡報技巧', '時間控制'],
+        datasets: [
+          {
+            data: [50, 20, 10, 10, 10],
+            backgroundColor: [
+              'rgb(66, 129, 164)',
+              'rgb(72, 169, 166)',
+              'rgb(228, 223, 218)',
+              'rgb(212, 180, 131)',
+              'rgb(193, 102, 107)'
+            ],
+            borderColor: '#000',
+            options: this.options
+          }
+        ]
+      },
+      dataI2: { // 競賽辦法.決賽評分項目.實作組
+        labels: ['創新想法', '作品穩定性', '作品實用性', '表達能力', '作品完整度'],
+        datasets: [
+          {
+            data: [30, 20, 20, 20, 10],
+            backgroundColor: [
+              'rgb(21, 96, 100)',
+              'rgb(0, 196, 154)',
+              'rgb(248, 225, 108)',
+              'rgb(255, 194, 180)',
+              'rgb(251, 143, 103)'
+            ],
+            borderColor: '#000',
+            options: this.options
+          }
+        ]
+      },
       cpt_caution: [ // 競賽辦法.注意事項
         {
           data: '本競賽分為「概念組」及「實作組」兩組，概念組或實作組僅能擇一參加，且須依規定繳交資料，如違反上述規定，則取消參賽資格。'
@@ -503,69 +594,45 @@ export default {
         },
         {
           data: '決賽方式與參加人數限制將遵循衛福部公告之「特殊傳染性肺炎防疫措施」，請隨時留意網站最新消息。'
+        },
+        {
+          data: '主辦單位保有隨時修改及終止本活動之權利，如有任何變更內容或詳細注意事項將以 email 通知，請留意訊息。'
+        },
+        {
+          data: '參賽作品非真正參賽人員不得列入，作品內文應由學生自行製作，指導老師只能以指導者之身分督導學生，不能代為製作，違者取消參賽資格。'
+        },
+        {
+          data: '參賽者繳交之電子資料，均不予退回，應自行備份，徵稿收件截止後，不得對文件進行調整或置換。'
+        },
+        {
+          data: '參賽者寄送之電子資料於徵稿收件截止後，經主辦單位確認該隊確實完成所有上傳事宜，將以電子郵件通知隊長。若完成上述動作後，逾三天仍未收到相關電子郵件通知，請來電詢問。'
         }
       ],
-      options: { // chartjs設定
-        responsive: true,
-        plugins: { // chartjs-plugin-datalabels設定
-          datalabels: {
-            formatter: (value, ctx) => {
-              const LEGEND_PERCENTAGE = value + '%' // 數值加'%'
-              return LEGEND_PERCENTAGE
-              // 預留程式碼區塊：可以計算所有數值轉換為百分比後的結果'
-              // let sum = 0
-              // const dataArr = ctx.chart.data.datasets[0].data
-              // dataArr.map(data => {
-              //   sum += data
-              //   return 0
-              // })
-              // const percentage = (value * 100 / sum).toFixed(2) + '%'
-            },
-            color: 'black', // labels設定字體顏色
-            align: 'end',
-            textAlign: 'center', // labels設定對齊樣式
-            font: {
-              weight: 'bold',
-              size: 12
-            },
-            offset: -6
-          }
+      award_info: [ // 競賽辦法.獎勵辦法
+        {
+          data: '獎品項目暫訂，主辦單位得依實際狀況更改之。'
+        },
+        {
+          data: '競賽得獎者，於推薦甄試高雄科技大學商業智慧學院各系，可列為多元表現加分項目。'
+        },
+        {
+          data: '所有參賽者，皆頒發參賽證明，可上傳作為推薦甄試能力佐證資料。'
         }
-      },
-      dataC: { // 競賽辦法.評分項目.概念組
-        labels: ['作品創作理念', '提案創新性', '市場應用可行性', '預期效益', '報告完整度'],
-        datasets: [
-          {
-            data: [10, 30, 30, 20, 10],
-            backgroundColor: [
-              'rgb(66, 129, 164)',
-              'rgb(72, 169, 166)',
-              'rgb(228, 223, 218)',
-              'rgb(212, 180, 131)',
-              'rgb(193, 102, 107)'
-            ],
-            borderColor: '#000',
-            options: this.options
-          }
-        ]
-      },
-      dataI: { // 競賽辦法.評分項目.實作組
-        labels: ['作品創作理念', '作品功能', '市場應用可行性', '成本分析', '實用價值/商業價值'],
-        datasets: [
-          {
-            data: [30, 20, 20, 20, 10],
-            backgroundColor: [
-              'rgb(21, 96, 100)',
-              'rgb(0, 196, 154)',
-              'rgb(248, 225, 108)',
-              'rgb(255, 194, 180)',
-              'rgb(251, 143, 103)'
-            ],
-            borderColor: '#000',
-            options: this.options
-          }
-        ]
-      }
+      ],
+      idea_award: [ // 競賽辦法.獎勵辦法
+        {
+          data: '特優一名，團體獎金10,000元、每人獎狀一只'
+        },
+        {
+          data: '優等一名，團體獎金5,000元、每人獎狀一只'
+        },
+        {
+          data: '佳作五名，團體獎金2,000元、每人獎狀一只'
+        },
+        {
+          data: '特別獎數名，每人獎狀一只'
+        }
+      ]
     }
   },
   methods: {
